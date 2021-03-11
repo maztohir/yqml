488bf5a5783c0a3eaba004a5b82bee2e59ccc5ce
maztohir <m.tohir@go-jek.com>
2021-03-11 22:57:04 +0700

    tohir | adding init in package

---
+++ setup.py
@@ -0,0 +1,32 @@
+from sys import version
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yqml",
+    version="0.0.1",
+    author="Muhamad Tohir",
+    author_email="maztohir@gmail.com",
+    description="YAML based query language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maztohir/yqml",
+    packages=setuptools.find_packages(exclude=("test",)),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords=[
+        'sql', 'python', 'query', 'yaml'
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'PyYAML==5.4.1'
+    ]
+)
---
+++ yqml/__init__.py
@@ -0,0 +1,1 @@
+from yqml.yqml import *
